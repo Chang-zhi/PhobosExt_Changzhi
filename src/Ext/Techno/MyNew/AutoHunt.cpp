d135771f26caa05e2432aeec6814352a1066bb6b
长长长长长智 <wzhangzhi0@gmail.com>
2026-05-24 17:12:56 +0800

    add deploy check

--- src/Ext/Techno/MyNew/AutoHunt.cpp
+++ src/Ext/Techno/MyNew/AutoHunt.cpp
@@ -15,11 +15,25 @@ void AutoHunt(TechnoClass* pThis)
 	{
 		// Debug::Log("[%s] AutoHunt: %s\n", pThis->GetType()->ID, "true");
 		Mission curMission = pThis->GetCurrentMission();
+
+		// 如果是部署状态，先解除部署
+		if (auto pInf = abstract_cast<InfantryClass*>(pThis))
+		{
+			if(pInf->IsDeployed())
+			pThis->ForceMission(Mission::Unload);
+			// pInf->ShouldDeploy = true;
+		}
+		if (auto pUnit = abstract_cast<UnitClass*>(pThis))
+		{
+			if(pUnit->Deployed)
+			pUnit->Undeploy();
+		}
+
 		if (pThis->GetCurrentMission() != Mission::Hunt && pThis->QueuedMission != Mission::Hunt)
 		{
 			// Debug::Log("[%s] AutoHunt: %s\n", pThis->GetType()->ID, "ForceMission");
-			pThis->ForceMission(Mission::Hunt);
-			// pThis->QueueMission(Mission::Hunt, true);
+			// pThis->ForceMission(Mission::Hunt);
+			pThis->QueueMission(Mission::Hunt, true);
 		}
 	}
 }
