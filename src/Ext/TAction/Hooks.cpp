3dff5575b043ccf697708e4e694296d3d0fada1d
长长长长长智 <wzhangzhi0@gmail.com>
2026-09-17 17:32:26 +0800

    [build] 从 Phobos 上游初始化 PhobosExt 工程骨架
    合并 2 条开发提交。
    
    - add base item
    - change Canary

---
+++ src/Ext/TAction/Hooks.cpp
@@ -0,0 +1,22 @@
+#include "Body.h"
+
+#include <Helpers/Macro.h>
+
+#include <HouseClass.h>
+
+#include <Utilities/Macro.h>
+
+DEFINE_HOOK(0x6DD8B0, TActionClass_Execute, 0x6)
+{
+	GET(TActionClass*, pThis, ECX);
+	GET_STACK(HouseClass*, pHouse, 0x4);
+	GET_STACK(ObjectClass*, pObject, 0x8);
+	GET_STACK(TriggerClass*, pTrigger, 0xC);
+	GET_STACK(CellStruct const*, pLocation, 0x10);
+
+	bool handled;
+
+	R->AL(TActionExt::Execute(pThis, pHouse, pObject, pTrigger, *pLocation, handled));
+
+	return handled ? 0x6DD910 : 0;
+}
---
+++ src/Ext/WarheadType/Body.cpp
@@ -0,0 +1,114 @@
+#include "Body.h"
+
+#include <BulletClass.h>
+#include <HouseClass.h>
+
+#include <Ext/Techno/Body.h>
+#include <Utilities/EnumFunctions.h>
+
+WarheadTypeExt::ExtContainer WarheadTypeExt::ExtMap;
+
+// =============================
+// load / save
+
+void WarheadTypeExt::ExtData::LoadFromINIFile(CCINIClass* const pINI)
+{
+	auto pThis = this->OwnerObject();
+	const char* pSection = pThis->ID;
+	INI_EX exINI(pINI);
+
+}
+
+template <typename T>
+void WarheadTypeExt::ExtData::Serialize(T& Stm)
+{
+	Stm
+
+		;
+}
+
+void WarheadTypeExt::ExtData::LoadFromStream(PhobosStreamReader& Stm)
+{
+	Extension<WarheadTypeClass>::LoadFromStream(Stm);
+	this->Serialize(Stm);
+}
+
+void WarheadTypeExt::ExtData::SaveToStream(PhobosStreamWriter& Stm)
+{
+	Extension<WarheadTypeClass>::SaveToStream(Stm);
+	this->Serialize(Stm);
+}
+
+bool WarheadTypeExt::LoadGlobals(PhobosStreamReader& Stm)
+{
+	return Stm.Success();
+}
+
+bool WarheadTypeExt::SaveGlobals(PhobosStreamWriter& Stm)
+{
+	return Stm.Success();
+}
+
+// =============================
+// container
+
+WarheadTypeExt::ExtContainer::ExtContainer() : Container("WarheadTypeClass") { }
+
+WarheadTypeExt::ExtContainer::~ExtContainer() = default;
+
+// =============================
+// container hooks
+
+DEFINE_HOOK(0x75D1A9, WarheadTypeClass_CTOR, 0x7)
+{
+	GET(WarheadTypeClass*, pItem, EBP);
+
+	WarheadTypeExt::ExtMap.TryAllocate(pItem);
+
+	return 0;
+}
+
+DEFINE_HOOK(0x75E5C8, WarheadTypeClass_SDDTOR, 0x6)
+{
+	GET(WarheadTypeClass*, pItem, ESI);
+
+	WarheadTypeExt::ExtMap.Remove(pItem);
+
+	return 0;
+}
+
+DEFINE_HOOK_AGAIN(0x75E2C0, WarheadTypeClass_SaveLoad_Prefix, 0x5)
+DEFINE_HOOK(0x75E0C0, WarheadTypeClass_SaveLoad_Prefix, 0x8)
+{
+	GET_STACK(WarheadTypeClass*, pItem, 0x4);
+	GET_STACK(IStream*, pStm, 0x8);
+
+	WarheadTypeExt::ExtMap.PrepareStream(pItem, pStm);
+
+	return 0;
+}
+
+DEFINE_HOOK(0x75E2AE, WarheadTypeClass_Load_Suffix, 0x7)
+{
+	WarheadTypeExt::ExtMap.LoadStatic();
+
+	return 0;
+}
+
+DEFINE_HOOK(0x75E39C, WarheadTypeClass_Save_Suffix, 0x5)
+{
+	WarheadTypeExt::ExtMap.SaveStatic();
+
+	return 0;
+}
+
+//DEFINE_HOOK_AGAIN(0x75DEAF, WarheadTypeClass_LoadFromINI, 0x5)// Section dont exist!
+DEFINE_HOOK(0x75DEA0, WarheadTypeClass_LoadFromINI, 0x5)
+{
+	GET(WarheadTypeClass*, pItem, ESI);
+	GET_STACK(CCINIClass*, pINI, 0x150);
+
+	WarheadTypeExt::ExtMap.LoadFromINI(pItem, pINI);
+
+	return 0;
+}
