06febe95769f61ff26c9a72c0742bf681f1e5dff
长长长长长智 <wzhangzhi0@gmail.com>
2026-09-17 17:32:26 +0800

    [Customization] 路径点文本：新增 `WaypointLabel` 绘制与动作接口
    合并 2 条开发提交。
    
    - add WayPointlabelclass

--- src/Phobos.cpp
+++ src/Phobos.cpp
@@ -1,19 +1,31 @@
 #include "Phobos.h"
+
+#include <Drawing.h>
+#include <SessionClass.h>
+#include <Unsorted.h>
+
+#include <Utilities/Debug.h>
 #include <Utilities/Patch.h>
 #include <Utilities/Macro.h>
+#include "Utilities/Parser.h"
 
-void Phobos::ExeRun()
-{
-	Patch::ApplyStatic();
-}
+HANDLE Phobos::hInstance = 0;
 
-bool __stdcall DllMain(HANDLE, DWORD dwReason, LPVOID)
+char Phobos::readBuffer[Phobos::readLength];
+wchar_t Phobos::wideBuffer[Phobos::readLength];
+
+bool Phobos::DisplayDamageNumbers = false;
+
+const wchar_t* Phobos::VersionDescription = L"Chang_zhi Custom Phobos Extension build #" _STR(BUILD_NUMBER) L". Please test the build before shipping.";
+
+// =============================
+// hooks
+
+bool __stdcall DllMain(HANDLE hInstance, DWORD dwReason, LPVOID v)
 {
+	if (dwReason == DLL_PROCESS_ATTACH)
+	{
+		Phobos::hInstance = hInstance;
+	}
 	return true;
-}
-
-DEFINE_HOOK(0x7CD810, ExeRun, 0x9)
-{
-	Phobos::ExeRun();
-	return 0;
 }
